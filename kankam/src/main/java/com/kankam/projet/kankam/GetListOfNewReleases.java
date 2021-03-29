998f88a09d68e9287a2a5c1f395a90a40456ed53
andy-kouekam <76410026+andy-kouekam@users.noreply.github.com>
2021-03-30 00:19:33 +0200

    Mapping

--- kankam/src/main/java/com/kankam/projet/kankam/GetListOfNewReleases.java
+++ kankam/src/main/java/com/kankam/projet/kankam/GetListOfNewReleases.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
+import com.neovisionaries.i18n.CountryCode;
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
@@ -15,25 +16,31 @@ import com.wrapper.spotify.requests.data.browse.GetListOfNewReleasesRequest;
 public class GetListOfNewReleases {
        
 	 public static String accessToken = ClientCredentialsExample.clientCredentials_Async();
+	 
 
 	    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
 	            .setAccessToken(accessToken)
 	            .build();
 	    private static final GetListOfNewReleasesRequest getListOfNewReleasesRequest = spotifyApi.getListOfNewReleases()
-//	          .country(CountryCode.FR)
-//	          .limit(10)
+	    	 .country(CountryCode.FR)
+	          .limit(10)
 //	          .offset(3)
 	            .build();
 
-	    public static void getListOfNewReleases_Sync() throws ParseException, org.apache.hc.core5.http.ParseException {
+	    public static Paging<AlbumSimplified> getListOfNewReleases_Sync() throws ParseException, org.apache.hc.core5.http.ParseException {
 	        try {
-	            final Paging<AlbumSimplified> albumSimplifiedPaging = getListOfNewReleasesRequest.execute();
-
-	            System.out.println("Total: " + albumSimplifiedPaging.getTotal());
+	        	
+	        	
+	                return   getListOfNewReleasesRequest.execute();
+	            
 	            
 	        } catch (IOException | SpotifyWebApiException e) {
 	            System.out.println("Error: " + e.getMessage());
+	            
 	        }
+	       
+	        return null;
+	       
 	    }
 
 	    public static void getListOfNewReleases_Async() {
@@ -43,8 +50,8 @@ public class GetListOfNewReleases {
 	            // Thread free to do other tasks...
 
 	            // Example Only. Never block in production code.
-	            final Paging<AlbumSimplified> albumSimplifiedPaging = pagingFuture.join();
-
+	            final Paging<AlbumSimplified> albumSimplifiedPaging = pagingFuture.join();		
+                
 	            System.out.println("Total: " + albumSimplifiedPaging.getTotal());
 	        } catch (CompletionException e) {
 	            System.out.println("Error: " + e.getCause().getMessage());
@@ -55,7 +62,7 @@ public class GetListOfNewReleases {
 	    }
 
 	    public static void main(String[] args) throws org.apache.hc.core5.http.ParseException, ParseException {
-	        getListOfNewReleases_Sync();
+	    	getListOfNewReleases_Sync();
 	        getListOfNewReleases_Async();
 	    }
 }
--- kankam/src/main/java/com/kankam/projet/kankam/HelloController.java
+++ kankam/src/main/java/com/kankam/projet/kankam/HelloController.java
@@ -5,18 +5,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
+import com.wrapper.spotify.model_objects.specification.Paging;
+
+
+
 
 @RestController
 @RequestMapping
 public class HelloController {
-	
-	
-	
-	 @GetMapping(value = "/")
-	 public void HelloController() throws ParseException, java.text.ParseException  {
-		 System.out.println("andy");
-		 GetListOfNewReleases.getListOfNewReleases_Sync();
-	      
-	    }
-	
+      
+    
+    @GetMapping(value = "/new-releases")
+    public String A(){
+        return "toto";
+    }
+     
+   
+    
+    @GetMapping(value = "/toto")
+    public Paging<AlbumSimplified> getInfos() throws ParseException, java.text.ParseException{
+    	
+        ClientCredentialsExample.clientCredentials_Async();
+        
+           
+                return GetListOfNewReleases.getListOfNewReleases_Sync();
+           
+        
+    }
 }
+
